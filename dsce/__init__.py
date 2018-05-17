b32f359bbc7ffe5bd4f18f44d501252dbbc4f563
Perakis Georgios <perakisgeorgios@gmail.com>
2018-05-17 16:12:19 +0300

    created __init__. Setup logger and data, model global variables.

---
+++ dsce/__init__.py
@@ -0,0 +1,196 @@
+import logging
+import os
+from logging import handlers
+from os.path import exists
+from os import makedirs
+
+
+def setup_logger(name):
+    """
+
+    :param name:
+    :return:
+    """
+
+    # create a logging format
+    formatter = logging.Formatter(
+        '%(asctime)s - PID:%(process)d - %(name)s.py:%(funcName)s:%(lineno)d - %(levelname)s - %(message)s')
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    # create INFO file handler
+    file_handler = handlers.RotatingFileHandler('{}.log'.format(name),
+                                                maxBytes=1024 * 1024 * 100,
+                                                backupCount=20)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    # create ERROR file handler
+    error_file_handler = handlers.RotatingFileHandler('{}.error.log'.format(name),
+                                                      maxBytes=1024 * 1024 * 100,
+                                                      backupCount=20)
+    error_file_handler.setLevel(logging.ERROR)
+    error_file_handler.setFormatter(formatter)
+    logger.addHandler(error_file_handler)
+
+    # create INFO stream handler
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+PARENT_DIRECTORY = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+
+DATA_DIR = os.path.join(PARENT_DIRECTORY, 'data')
+MODELS_DIR = os.path.join(PARENT_DIRECTORY, 'models')
+
+RAW_DATA_DIR = os.path.join(DATA_DIR, 'raw')
+PROCESSED_DATA_DIR =  os.path.join(DATA_DIR, 'processed')
+OUTFILE_DATA_DIR =  os.path.join(DATA_DIR, 'outfile')
+
+# if the folders don't exist, create them.
+if not exists(DATA_DIR):
+    makedirs(DATA_DIR)
+
+if not exists(MODELS_DIR):
+    makedirs(MODELS_DIR)
+
+if not exists(RAW_DATA_DIR):
+    makedirs(RAW_DATA_DIR)
+
+if not exists(PROCESSED_DATA_DIR):
+    makedirs(PROCESSED_DATA_DIR)
+
+if not exists(OUTFILE_DATA_DIR):
+    makedirs(OUTFILE_DATA_DIR)
+
+CONTRACTION_MAP = {
+    "ain't": "is not",
+    "aren't": "are not",
+    "can't": "cannot",
+    "can't've": "cannot have",
+    "'cause": "because",
+    "could've": "could have",
+    "couldn't": "could not",
+    "couldn't've": "could not have",
+    "didn't": "did not",
+    "doesn't": "does not",
+    "don't": "do not",
+    "hadn't": "had not",
+    "hadn't've": "had not have",
+    "hasn't": "has not",
+    "haven't": "have not",
+    "he'd": "he would",
+    "he'd've": "he would have",
+    "he'll": "he will",
+    "he'll've": "he he will have",
+    "he's": "he is",
+    "how'd": "how did",
+    "how'd'y": "how do you",
+    "how'll": "how will",
+    "how's": "how is",
+    "I'd": "I would",
+    "I'd've": "I would have",
+    "I'll": "I will",
+    "I'll've": "I will have",
+    "I'm": "I am",
+    "I've": "I have",
+    "i'd": "i would",
+    "i'd've": "i would have",
+    "i'll": "i will",
+    "i'll've": "i will have",
+    "i'm": "i am",
+    "i've": "i have",
+    "isn't": "is not",
+    "it'd": "it would",
+    "it'd've": "it would have",
+    "it'll": "it will",
+    "it'll've": "it will have",
+    "it's": "it is",
+    "let's": "let us",
+    "ma'am": "madam",
+    "mayn't": "may not",
+    "might've": "might have",
+    "mightn't": "might not",
+    "mightn't've": "might not have",
+    "must've": "must have",
+    "mustn't": "must not",
+    "mustn't've": "must not have",
+    "needn't": "need not",
+    "needn't've": "need not have",
+    "o'clock": "of the clock",
+    "oughtn't": "ought not",
+    "oughtn't've": "ought not have",
+    "shan't": "shall not",
+    "sha'n't": "shall not",
+    "shan't've": "shall not have",
+    "she'd": "she would",
+    "she'd've": "she would have",
+    "she'll": "she will",
+    "she'll've": "she will have",
+    "she's": "she is",
+    "should've": "should have",
+    "shouldn't": "should not",
+    "shouldn't've": "should not have",
+    "so've": "so have",
+    "so's": "so as",
+    "that'd": "that would",
+    "that'd've": "that would have",
+    "that's": "that is",
+    "there'd": "there would",
+    "there'd've": "there would have",
+    "there's": "there is",
+    "they'd": "they would",
+    "they'd've": "they would have",
+    "they'll": "they will",
+    "they'll've": "they will have",
+    "they're": "they are",
+    "they've": "they have",
+    "to've": "to have",
+    "wasn't": "was not",
+    "we'd": "we would",
+    "we'd've": "we would have",
+    "we'll": "we will",
+    "we'll've": "we will have",
+    "we're": "we are",
+    "we've": "we have",
+    "weren't": "were not",
+    "what'll": "what will",
+    "what'll've": "what will have",
+    "what're": "what are",
+    "what's": "what is",
+    "what've": "what have",
+    "when's": "when is",
+    "when've": "when have",
+    "where'd": "where did",
+    "where's": "where is",
+    "where've": "where have",
+    "who'll": "who will",
+    "who'll've": "who will have",
+    "who's": "who is",
+    "who've": "who have",
+    "why's": "why is",
+    "why've": "why have",
+    "will've": "will have",
+    "won't": "will not",
+    "won't've": "will not have",
+    "would've": "would have",
+    "wouldn't": "would not",
+    "wouldn't've": "would not have",
+    "y'all": "you all",
+    "y'all'd": "you all would",
+    "y'all'd've": "you all would have",
+    "y'all're": "you all are",
+    "y'all've": "you all have",
+    "you'd": "you would",
+    "you'd've": "you would have",
+    "you'll": "you will",
+    "you'll've": "you will have",
+    "you're": "you are",
+    "you've": "you have"
+}
