2ecff3acecf0c062d412c0c7395529aa74175773
Perakis Georgios <perakisgeorgios@gmail.com>
2018-05-24 15:25:49 +0300

    created generate_graph_from_text()

--- rpcc/load_data.py
+++ rpcc/load_data.py
@@ -7,8 +7,11 @@ from collections import Counter
 import networkx as nx
 import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit
-
+from sklearn.feature_extraction.text import CountVectorizer
 from rpcc import RAW_DATA_DIR, PROCESSED_DATA_DIR
+from nltk import sent_tokenize
+from more_itertools import windowed, flatten
+from itertools import combinations
 
 
 class DataLoader:
@@ -277,6 +280,68 @@ class DataLoader:
 
         else:
             raise NotImplementedError('Must load Node INFO first.')
+
+    @staticmethod
+    def generate_graph_from_text(text, window_size=3, directed=True, stop_word='english'):
+        """
+        This method creates a graph from the words of a text. At first splits the text in sentences
+        and then parses each sentence through a  sliding window, generating a graph by joining the window tokens with
+        some weights.
+
+        :type text: str
+        :type window_size: int
+        :type directed: bool
+        :type stop_word: str
+        :param text: A document of any size.
+        :param window_size: The size of the sliding window that we will use in order to generate the nodes and edges.
+        :param directed: Whether we will created a directed or undirected graph.
+        :param stop_word: Stopwords for stop word removal.
+        :return: A networkX graph.
+        """
+        assert directed in [True, False]
+        assert window_size in range(1, 6)
+
+        # splitting the text into sentences.
+        sentences = sent_tokenize(text)
+
+        # instantiating a tokenizer in order to tokenize each sentence.
+        sent2tokens_tokenizer = CountVectorizer(stop_words=stop_word).build_analyzer()
+
+        if directed:
+            # instantiating the Directed graph
+            G = nx.DiGraph()
+        else:
+            # instantiating the Undirted graph
+            G = nx.Graph()
+
+        # for each sentence split in tokens
+        for sentence in sentences:
+            tokens = sent2tokens_tokenizer(sentence)
+
+            # All the magic is here!  Creates the weights indices.
+            # For example for window_size = 5 the output is weight_index = [1, 2, 3, 4, 1, 2, 3, 1, 2, 1]
+            # The weight index remain the same for all windows due to the fact that the window size is stable.
+            weight_index = list(flatten([list(range(1, i + 1)) for i in range(window_size - 1, 0, -1)]))
+
+            for window in windowed(tokens, window_size):
+                # for the tokens in the window take all the combinations.
+                # Eg: Tokens: [tok1, tok2, tok3, tok4] the result will be:
+                # [tok1, tok2], [tok1, tok3], [tok1, tok4], [tok2, tok3], [tok2, tok4], [tok3, tok4]
+                for num, comb in enumerate(combinations(window, 2)):
+                    # create the actual weight for each combination: eg: 1, 1/2, 1/3, 1/4, etc
+                    weight = 1 / weight_index[num]
+
+                    # if there is already an edge between the two text tokens, add more weight between the edges.
+                    if G.has_edge(comb[0], comb[1]):
+                        G[comb[0]][comb[1]]['weight'] += weight
+                    else:
+                        # there is no edge, so we create an edge between them.
+                        G.add_edge(comb[0], comb[1], weight=weight)
+
+            # removing any self loop edges.
+            G.remove_edges_from(G.selfloop_edges())
+
+        return G
 
     def run_data_preparation(self, val_size=0.2):
         """
